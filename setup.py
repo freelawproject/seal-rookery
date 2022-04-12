b1cd78a970ced39d0be94333c21e4036c23457cb
William Palin <bill@free.law>
2022-04-12 17:39:04 -0400

    Version Bump v2.1.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools.command.install_lib import install_lib as _install_lib
 
 SETUP_DIR = os.path.dirname(os.path.abspath(__file__))
 
-VERSION = "2.1.2"
+VERSION = "2.1.3"
 AUTHOR = "Free Law Project"
 EMAIL = "info@free.law"
 NAME = "seal_rookery"
