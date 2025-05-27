be8c2bdec37cddceb7ea05017da6e89e1872c002
Adam Johnson <me@adamj.eu>
2025-05-27 11:12:06 +0100

    test: Improve tests

--- test.py
+++ test.py
@@ -1,26 +1,48 @@
+import sys
 import unittest
+from io import StringIO
+from pathlib import Path
+from unittest import mock
+
+from seal_rookery import _SealRookery, seals_data, seals_root
 
 
-class PackagingTests(unittest.TestCase):
-    """
-    Some simple tests to make sure the package works. Does not test functionality.
-    """
+class SealsDataTests(unittest.TestCase):
+    @mock.patch("seal_rookery.open", side_effect=FileNotFoundError)
+    def test_data_missing(self, _mock_open):
+        out = StringIO()
+        rookery = _SealRookery()
 
-    def test_does_not_break_cl(self):
-        """
-        Can we do some import statements like in the CL project? Test if we
-        are about to break Courtlistener.
-        """
-        try:
-            # currently from courtlistener.cl.scrapers.tasks
-            from seal_rookery import seals_data, seals_root
+        with mock.patch.object(sys, "stdout", new=out):
+            data = rookery.seals_data
 
-            self.assertTrue(seals_data["ca1"]["has_seal"])
+        self.assertEqual(data, {})
+        output = out.getvalue()
+        self.assertTrue(
+            output.startswith("Seals json missing or not generated yet: ")
+        )
 
-            self.assertIsNotNone(seals_root)
+    def test_success(self):
+        self.assertEqual(
+            seals_data["ca1"],
+            {
+                "has_seal": True,
+                "hash": "9cf025a005123030a5adb005c1eb6bf2e6a7de10a1e21f85f48812c85e7e92b0",
+                "name": "Court of Appeals for the First Circuit",
+                "notes": "Completed as part of initial version.",
+            },
+        )
 
-        except ImportError:
-            self.fail("Couldn't import seals_data and seals_root like in CL")
+
+class SealsRootTests(unittest.TestCase):
+    def test_seals_root(self):
+        self.assertIsInstance(seals_root, str)
+        path = Path(seals_root)
+        self.assertTrue(path.exists())
+        self.assertTrue(path.is_dir())
+        ca1_svg = path / "orig/ca1.svg"
+        self.assertTrue(ca1_svg.exists())
+        self.assertGreater(ca1_svg.stat().st_size, 0)
 
 
 if __name__ == "__main__":
